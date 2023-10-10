58cfde54f0393d9ecb0bd4f0484941e97e99ed9d
GYEONGROK11 <gpol123@naver.com>
2023-10-10 14:46:54 +0900

    1010

---
+++ Java2023/first/src/main/java/com/green/honjahaebogy/q3.java
@@ -0,0 +1,17 @@
+package com.green.honjahaebogy;
+
+public class q3 {
+    /* 3,6,9게임
+i=1
+i=2
+i=3 짝
+i=4
+i=5
+i=6 짝
+i=30 짝
+i=33 짝짝
+ */
+    public static void main(String[] args) {
+
+    }
+}
