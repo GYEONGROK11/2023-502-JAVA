2877fc2f2da06cd6899b20c3fe9f6f4d292034a2
GYEONGROK11 <gpol123@naver.com>
2023-11-08 09:48:17 +0900

    1108_1

--- Java2023/first/src/main/java/com/green/Day5/ch4/RandomValMission2.java
+++ Java2023/first/src/main/java/com/green/Day5/ch4/RandomValMission2.java
@@ -2,13 +2,13 @@ package com.green.day5.ch4;
 
 public class RandomValMission2 {
     public static void main(String[] args) {
-        final int MIN = 10, MAX = 100;
+        final int MIN = 10, MAX = 50;
 
 
         //리터럴 더이상 하지않고 min max  상수만 이용하여  MIN~MAX만 나올수있는 랜덤 숫자를 만드는 로직을 구현
 
 
-        for(int i=0;i<1000;i++){
+        for(int i=0;i<50;i++){
             int rVal = (int) (Math.random() * (MAX-MIN+1)) + MIN; //10~50
             System.out.println("val : " + rVal);
             if(rVal < MIN || rVal > MAX){
